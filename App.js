8103355871d71243f6087e433e839aa679c676a4
Jorge Suarez Chiquito <jsuarez@hugeinc.com>
2018-05-03 13:26:47 -0500

    Created TaskList Component

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import TaskList from './TaskList';
 
 export default class App extends React.Component {
+  state = {
+    todos: [
+      {
+        task: 'Learn React Native'
+      }
+    ]
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <TaskList  />
       </View>
     );
   }
---
+++ TaskList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+class TaskList extends React.Component {
+  render() {
+    return (
+      <Text>Hi, this is th jse TaskList!</Text>
+    );
+  }
+}
+
+export default TaskList;
