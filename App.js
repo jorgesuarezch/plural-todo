a43143daa501bc3bf7ace14b414a7bc5dcb27b09
Jorge Suarez Chiquito <jsuarez@hugeinc.com>
2018-05-03 14:19:46 -0500

    Created and populated datasource using cloneWithRows. Added ListView

--- App.js
+++ App.js
@@ -1,29 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import TaskList from './TaskList';
 
 export default class App extends React.Component {
-  state = {
-    todos: [
-      {
-        task: 'Learn React Native'
-      }
-    ]
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      todos: [
+        {
+          task: 'Learn React Native'
+        },
+        {
+          task: 'Learn Redux'
+        }
+      ],
+    }
   }
   render() {
     return (
-      <View style={styles.container}>
-        <TaskList  />
-      </View>
+      <TaskList
+        todos={this.state.todos}
+      />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- TaskList.js
+++ TaskList.js
@@ -1,12 +1,52 @@
 import React from 'react';
-import { Text } from 'react-native';
+import PropTypes from 'prop-types';
+import {
+  ListView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 40,
+  }
+});
 
 class TaskList extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    const dataSource = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2,
+    });
+
+    this.state = {
+      dataSource: dataSource.cloneWithRows(props.todos),
+    }
+  }
+
+  renderRow(todo) {
+    return (
+      <Text>{todo.task}</Text>
+    );
+  }
+
   render() {
     return (
-      <Text>Hi, this is th jse TaskList!</Text>
+      <View style={styles.container}>
+        <ListView
+          key={this.props.todos}
+          dataSource={this.state.dataSource}
+          renderRow={this.renderRow.bind(this)}
+        />
+      </View>
     );
   }
 }
+
+TaskList.propTypes = {
+  todos: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default TaskList;
